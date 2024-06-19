20c7a03fa871ef9cc2051c86f85479c7a4d81f78
Donovan Barrientos <donovan_ba@outlook.com>
2024-06-19 12:42:04 -0600

    added crud heroes

---
+++ src/app/heroes/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styles: ``
+})
+export class ConfirmDialogComponent {
+
+    constructor(private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+      @Inject(MAT_DIALOG_DATA) public data: Hero) { }
+
+
+      onNoClick(){
+        this.dialogRef.close(false);
+      }
+
+      onConfirm(){
+        this.dialogRef.close(true)
+      }
+}
+
